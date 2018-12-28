b2f843cc66a0ae5543f87c0669c790d1bb087f53
LazukinPavel <lazukinpo@gmail.com>
2018-12-28 20:28:30 +0300

    Using PureComponent

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Scoreboard on React. Based on TeamTreeHouse React Tutorials.
+## Scoreboard on React. Based on TeamTreeHouse React Tutorials.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,22 +1,23 @@
-import React from 'react';
-
+import React, {PureComponent} from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
+class Player extends PureComponent {
+  render() {
+      return (
+      <div className="player">
+        <span className="player-name">
+          <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>✖</button>
+          { this.props.name }
+        </span>
 
-      <Counter 
-      	score={props.score}
-      	scoreChange={props.scoreChange}
-      	index={props.index}
-      />
-    </div>
-  );
+        <Counter 
+          score={this.props.score}
+          scoreChange={this.props.scoreChange}
+          index={this.props.index}
+        />
+      </div>
+    );
+  }  
 }
 
 export default Player;
